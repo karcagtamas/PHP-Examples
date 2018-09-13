1f54b76de630633f21f9fe681780a90cbd159639
karcagtamas <karcagtamas@gmail.com>
2018-09-13 09:22:12 +0200

    Regisztráció befejezése, stlye

--- 2018-09-13_01.php
+++ 2018-09-13_01.php
@@ -8,19 +8,38 @@
     <title>Regisztráció validácó</title>
 </head>
 <body>
+    <div id="box">
     <?php
         /* foreach ($_POST as $key => $value) {
             echo "<p>Kulcs: $key - Érték: $value</p>";
         } */
 
-        if ($_POST['name'] != "")
-        {
 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST')  
+        {
+            if ($_POST['name'] != "")
+            {
+                if ($_POST['pass'] == $_POST['pass2'])
+                {
+                    echo "<p class='succes'>Sikeres regisztráció!</p>";
+                }
+                else
+                {
+                    echo "<p class='error'>A két jelszó nem egyezik!</p>";
+                }
+            }
+            else
+            {
+                echo "<p class='error'>A név megadása kötelező!</p>";
+            }
         }
         else
         {
-            echo "<p class='error'>A név megadása kötelező!";
+            echo "<p class='error'>A név megadása kötelező!</p>";
+            echo "<a href='2018-09-13_01.html'>Vissza lépés az Űrlaphoz</a>";
+            die;
         }
     ?>
+    </div>
 </body>
 </html>
