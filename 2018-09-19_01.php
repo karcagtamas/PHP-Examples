7cce52684f1c969c527146ff28c5875a4b62df29
karcagtamas <karcagtamas@gmail.com>
2018-09-19 09:39:45 +0200

    téglalap

---
+++ 2018-09-19_01.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="2018-09-19_01.css">
+    <title>Téglalap</title>
+</head>
+<body>
+    <fieldset id="input">
+        <legend>Téglalap</legend>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+            <label>A:</label>
+            <input type="number" name="A" id="A">
+            <br> <br>
+            <label>B:</label>
+            <input type="number" name="B" id="B">
+            <br><br>
+            <div id="buttonframe">
+                <input type="submit" value="Számítás" id="button">
+            </div>
+            
+        </form>
+    </fieldset>
+    <br>
+    <?php 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            $A = htmlspecialchars($_POST['A']); 
+            $B = htmlspecialchars($_POST['B']);
+            $Kerület = ($A + $B) * 2;
+            $Terület = $A * $B;
+            echo "<fieldset id='data'>";
+            echo "<legend>Adatok</legend>";
+            echo "Kerület: $Kerület px";
+            echo "<br> <br>";
+            echo "Terület: $Terület px";
+            echo "</fieldset>";
+            echo "<br>";
+            echo "<div id='rectangle' style='width:".$A."px; height:".$B."px'></div>";
+        }
+    ?>
+</body>
+</html>
