c5070b6a92af62bfd08463106fcf2db6a066ccfc
karcagtamas <karcagtamas@gmail.com>
2018-10-10 09:42:42 +0200

    fájlkezelés

---
+++ 2018-10-10/2018-10-10_01.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        if (isset($_POST['user']))
+        {
+            $f = fopen("users.txt", "r");
+            $belepve = 0;
+            while(!feof($f) && $belepve == 0)
+            {
+                $sor = fgets($f);
+                //$tomb = explode(":", $sor);
+                list($fuser, $fpass) = explode(":",$sor);
+                if ($_POST["user"] == trim($fuser))
+                {
+                    if ($_POST["password"] == trim($fpass))
+                    {
+                        $belepve = 1;
+                    }
+                    else
+                    {
+                        $belepve = -1;
+                    }
+                }
+            }
+            var_dump($belepve);
+            fclose($f);
+        } 
+    ?>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <label>Usernév:</label><input type="text" name="user">
+        <label>Jelszó:</label><input type="password" name="password">
+        <input type="submit" value="Belépés">
+    </form>
+</body>
+</html>
